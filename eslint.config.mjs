ccfd20a9995663be57f92c69937e1d33032353eb
Segev Finer <segev208@gmail.com>
2025-03-29 13:24:40 +0300

    Update deps

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default defineConfig([
+    {
+        files: ["**/*.ts"],
+    },
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
+
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 2022,
+            sourceType: "module",
+        },
+
+        rules: {
+            "@typescript-eslint/naming-convention": ["warn", {
+                selector: "import",
+                format: ["camelCase", "PascalCase"],
+            }],
+
+            curly: "warn",
+            eqeqeq: "warn",
+            "no-throw-literal": "warn",
+            semi: "warn",
+        },
+    }
+]);
